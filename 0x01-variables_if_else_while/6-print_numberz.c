49f16a8a1f11973dbb10ab9c8a67df5b5d285629
Asher <asherwhite17@gmail.com>
2022-10-14 16:18:18 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int q;
+
+	q = 0;
+	while
+		(q  < 10) {
+			putchar(q = '0');
+			q++;
+		}
+	putchar('\n');
+	return (0);
+}
+
