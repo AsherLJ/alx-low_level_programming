696cf47ca09de71d1006481aeae94fa9741f3576
Asher <asherwhite17@gmail.com>
2022-10-14 16:41:47 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main -Entry
+ * Return: Always
+ */
+int main(void)
+{
+	char q;
+	int p;
+
+	q = 'a';
+	while
+		(p < 10) {
+			putchar(p + '0');
+			p++;
+		}
+	while
+		(q <= 'f') {
+			putchar(q);
+			q++;
+		}
+	putchar('\n');
+	return (0);
+
+
+}
