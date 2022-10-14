4415e8758e72ef5950c3031047cc7241141e2349
Asher <asherwhite17@gmail.com>
2022-10-14 15:46:30 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char q;
+	char p;
+
+	q = 'a';
+	p = 'A';
+		while
+			(q <= 'z') {
+				putchar(q);
+				q++;
+			}
+		while
+			(p <= 'Z') {
+				putchar(p);
+				p++;
+			}
+		putchar('\n');
+		return (0);
+}
