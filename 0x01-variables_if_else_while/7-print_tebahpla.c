6c39ce80a649d738e1d305072373168e0a5a1b0a
Asher <asherwhite17@gmail.com>
2022-10-14 16:30:41 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char q;
+
+	q = 'z';
+	while
+		(q >= 'a') {
+			putchar(q);
+			q--;
+		}
+	putchar('\n');
+	return (0);
+}
