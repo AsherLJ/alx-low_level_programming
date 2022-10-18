d51ff9fc6359d1b46ec17def132855e382f494b0
Asher <asherwhite17@gmail.com>
2022-10-18 16:37:21 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num - 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
@@ -15,7 +15,7 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num + multi;
+			prod = num + mult;
 
 			if (prod <= 9)
 				_putchar(' ');
